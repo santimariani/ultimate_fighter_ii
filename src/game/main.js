27213e049d5d48706a332d36533f3fd8644fec50
Santiago Mariani <santi@gospartanburg.com>
2024-07-11 15:40:50 -0400

    first commit

---
+++ src/game/main.js
@@ -0,0 +1,35 @@
+import { Boot } from './scenes/Boot';
+import { Game } from './scenes/Game';
+import { GameOver } from './scenes/GameOver';
+import { MainMenu } from './scenes/MainMenu';
+import Phaser from 'phaser';
+import { Preloader } from './scenes/Preloader';
+
+// Find out more information about the Game Config at:
+// https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+const config = {
+    type: Phaser.AUTO,
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: window.innerWidth,
+        height: window.innerHeight
+    },
+    scene: [
+        Boot,
+        Preloader,
+        MainMenu,
+        Game,
+        GameOver
+    ]
+};
+
+const StartGame = (parent) => {
+
+    return new Phaser.Game({ ...config, parent });
+
+}
+
+export default StartGame;
